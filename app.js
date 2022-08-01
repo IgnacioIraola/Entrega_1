c8864ca432106cac47da27c319c20b3a139d8508
IgnacioIraola <iraolai@hotmail.com>
2022-07-31 21:43:47 -0300

    Ejercicio terminado

---
+++ app.js
@@ -0,0 +1,7 @@
+let ingresoDeEdad = (Number(prompt ("Ingrese su edad")))
+
+if (ingresoDeEdad < 18){
+    alert("No puedes ingresar sos menor de edad")
+}else{
+    alert("Sos mayor de edad, Bienvenido")
+}
